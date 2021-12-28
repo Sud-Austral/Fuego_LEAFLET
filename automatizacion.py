f6103a1b6e8967cd2e020312a5291ad527a2ddf9
lmonsalve22 <lmonsalve22@gmail.com>
2021-12-28 15:56:21 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -4,6 +4,13 @@ import pandas as pd
 import sys
 import datetime
 
+fuentes = [["https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_7d.csv",
+            "MODIS"],
+            ["https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_South_America_7d.csv",
+            "SUOMI"],
+            ["https://firms.modaps.eosdis.nasa.gov/data/active_fire/noaa-20-viirs-c2/csv/J1_VIIRS_C2_South_America_7d.csv",
+            "J1"]]
+            
 def proceso():
     url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_7d.csv"
     df = pd.read_csv("https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_24h.csv")
