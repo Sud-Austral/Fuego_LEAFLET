9d481533c6516ba52518c75713ba296f8c80cce8
lmonsalve22 <lmonsalve22@gmail.com>
2021-12-28 16:03:53 -0300

    asd

--- automatizacion.py
+++ automatizacion.py
@@ -10,17 +10,22 @@ fuentes = [["https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/cs
             "SUOMI"],
             ["https://firms.modaps.eosdis.nasa.gov/data/active_fire/noaa-20-viirs-c2/csv/J1_VIIRS_C2_South_America_7d.csv",
             "J1"]]
-            
-def proceso():
-    url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_7d.csv"
+
+def descarga(fuente):
+    url = fuente[0]
+    #url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_7d.csv"
     df = pd.read_csv("https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_24h.csv")
     dfDate = df[df["acq_date"] == datetime.datetime.now().strftime("%Y-%m-%d")]
     if(len(dfDate) > 0):
         dfDate = df
     dfLat = dfDate[dfDate["latitude"] < -16.5]
     dfLat2 = dfLat[dfLat["longitude"] < -69.5]
-    dfLat2.to_csv("Data/Puntos_Diarios.csv")
+    dfLat2.to_csv(f"Data/{fuente[0]}/Puntos_Diarios_{fuente[0]}.csv")
     return dfLat2
+
+def proceso():
+    for i in fuentes:
+        descarga(i)
 
 if __name__ == '__main__':
     try:
