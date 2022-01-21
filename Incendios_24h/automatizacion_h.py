11c66042fcd8e16807d390e2e5ac9fae727f1e1f
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2022-01-21 17:49:03 -0600

    Commit

--- Incendios_24h/automatizacion_h.py
+++ Incendios_24h/automatizacion_h.py
@@ -5,8 +5,12 @@ import sys
 import datetime
 import json
 
-fuentesLink = 'https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true'
-fuentePuntos = ['MODIS_24h', 'SUOMI_24h', 'J1_24h']
+fuentes = [["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true",
+            "MODIS_diario"],
+            ["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true",
+            "SUOMI_diario"],
+            ["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true",
+            "J1_diario"]]
 
 def regiones(region):
     
@@ -27,25 +31,39 @@ def provincias(prov):
     return data['PROVINCIA'][indx]
 
 def descarga(fuente):
-    print(fuente)
-    print(fuentesLink)
+    url = fuente[0]
+    dataFuente = ''
+
+    if(fuente[1] == 'MODIS_24h'):
+        dataFuente = 'MODIS'
+
+    if(fuente[1] == 'SUOMI_24h'):
+        dataFuente = 'SUOMI'
+
+    if(fuente[1] == 'J1_24h'):
+        dataFuente = 'J1'
+
+    print('FUENTE 24: ' + str(dataFuente))   
     
-    url = fuentesLink
+    today = str(datetime.datetime.today())[0:10]
 
     df = pd.read_excel(url)
+    df = df[df['acq_date'] <= today]
 
     df['NOM_REGION'] = df['REGION'].apply(lambda x: regiones(x))
     df['NOM_PROVINCIA'] = df['PROVINCIA'].apply(lambda x: provincias(x))
 
-    df = df[df['Fuente'] == fuente]
+    df = df[df['Fuente'] == dataFuente]
 
     dfLat2 = df
 
     # AQUÍ SE PODRÍA AGREGAR LA INFORMACIÓN CALLE, COMUNA, PROVINCIA, REGIÓN.
     # CALLE, COMUNA, PROVINCIA, REGIÓN (INCLUIR JSON)
 
-    dfLat2.to_csv(f"Incendios_24h/Data/{fuente}/Puntos_Diarios_{fuente}.csv")
-    dfLat2.to_csv(f"Incendios_24h/Data_Legacy/{fuente}/Puntos_Diarios_{fuente}_{datetime.datetime.now().strftime('%Y-%m-%d')}.csv")
+    dfLat2.to_csv(f"Incendios_24h/Data/{fuente[1]}/Puntos_Diarios_{fuente[1]}.csv")
+    dfLat2.to_csv(f"Incendios_24h/Data_Legacy/{fuente[1]}/Puntos_Diarios_{fuente[1]}_{datetime.datetime.now().strftime('%Y-%m-%d')}.csv")
+    
+
     
     return dfLat2
 
@@ -61,16 +79,15 @@ def getJSON(fuente):
         f = {'acq_date': j["acq_date"],"lat":j["latitude"],"lng":j["longitude"], "location": j["Locacion"], "region": j["NOM_REGION"], "provincia": j["NOM_PROVINCIA"], "comuna": j["Comuna"]}
         features2.append(f.copy())
     salida = {"type":"FeatureCollection","features":features}
-    with open(f'Incendios_24h/Data/{fuente}/heatmap_{fuente}.json', 'w') as file:
+    with open(f'Incendios_24h/Data/{fuente[1]}/heatmap_{fuente[1]}.json', 'w') as file:
         json.dump(salida, file, indent=4)
-    with open(f'Incendios_24h/Data/{fuente}/data_{fuente}.json', 'w') as file:
+    with open(f'Incendios_24h/Data/{fuente[1]}/data_{fuente[1]}.json', 'w') as file:
         json.dump(features2, file, indent=4)
     return True
 
 def proceso():
-    for i in fuentePuntos:
+    for i in fuentes:
         #descarga(i)
-
         getJSON(i)
 
 if __name__ == '__main__':
