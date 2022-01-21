c216516fe9a2f226d78cbd895d4ea202b6ce6d36
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2022-01-21 17:34:21 -0600

    Commit

--- Incendios_24h/automatizacion_h.py
+++ Incendios_24h/automatizacion_h.py
@@ -5,12 +5,8 @@ import sys
 import datetime
 import json
 
-enlaces = [["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true1",
-            "MODIS_diario"],
-            ["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true2",
-            "SUOMI_diario"],
-            ["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true3",
-            "J1_diario"]]
+fuentesLink = 'https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true'
+fuentePuntos = ['MODIS_24h', 'SUOMI_24h', 'J1_24h']
 
 def regiones(region):
     
@@ -31,22 +27,22 @@ def provincias(prov):
     return data['PROVINCIA'][indx]
 
 def descarga(fuente):
-    url = fuente[0]
+    url = fuentesLink
 
     df = pd.read_excel(url)
 
     df['NOM_REGION'] = df['REGION'].apply(lambda x: regiones(x))
     df['NOM_PROVINCIA'] = df['PROVINCIA'].apply(lambda x: provincias(x))
 
-    df = df[df['Fuente'] == fuente[1]]
+    df = df[df['Fuente'] == fuente]
 
     dfLat2 = df
 
     # AQUÍ SE PODRÍA AGREGAR LA INFORMACIÓN CALLE, COMUNA, PROVINCIA, REGIÓN.
     # CALLE, COMUNA, PROVINCIA, REGIÓN (INCLUIR JSON)
 
-    dfLat2.to_csv(f"Incendios_24h/Data/{fuente[1]}/Puntos_Diarios_{fuente[1]}.csv")
-    dfLat2.to_csv(f"Incendios_24h/Data_Legacy/{fuente[1]}/Puntos_Diarios_{fuente[1]}_{datetime.datetime.now().strftime('%Y-%m-%d')}.csv")
+    dfLat2.to_csv(f"Incendios_24h/Data/{fuente}/Puntos_Diarios_{fuente}.csv")
+    dfLat2.to_csv(f"Incendios_24h/Data_Legacy/{fuente}/Puntos_Diarios_{fuente}_{datetime.datetime.now().strftime('%Y-%m-%d')}.csv")
     
     return dfLat2
 
@@ -62,16 +58,16 @@ def getJSON(fuente):
         f = {'acq_date': j["acq_date"],"lat":j["latitude"],"lng":j["longitude"], "location": j["Locacion"], "region": j["NOM_REGION"], "provincia": j["NOM_PROVINCIA"], "comuna": j["Comuna"]}
         features2.append(f.copy())
     salida = {"type":"FeatureCollection","features":features}
-    with open(f'Incendios_24h/Data/{fuente[1]}/heatmap_{fuente[1]}.json', 'w') as file:
+    with open(f'Incendios_24h/Data/{fuente}/heatmap_{fuente}.json', 'w') as file:
         json.dump(salida, file, indent=4)
-    with open(f'Incendios_24h/Data/{fuente[1]}/data_{fuente[1]}.json', 'w') as file:
+    with open(f'Incendios_24h/Data/{fuente}/data_{fuente}.json', 'w') as file:
         json.dump(features2, file, indent=4)
     return True
 
 def proceso():
-    for i in enlaces:
+    for i in fuentePuntos:
         #descarga(i)
-        print(i)
+
         getJSON(i)
 
 if __name__ == '__main__':
