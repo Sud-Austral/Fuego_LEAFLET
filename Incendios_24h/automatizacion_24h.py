b4510f1d80b4b409ce7b657dd3c41395cacad100
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2022-01-21 02:09:19 -0600

    Commit

---
+++ Incendios_24h/automatizacion_24h.py
@@ -0,0 +1,68 @@
+import requests
+import json
+import pandas as pd
+import sys
+import datetime
+import json
+
+fuentes = [["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true'",
+            "MODIS_24h"],
+            ["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true'",
+            "SUOMI_24h"],
+            ["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true'",
+            "J1_24h"]]
+
+def descarga(fuente):
+    url = fuente[0]
+    print(url)   
+    #url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_7d.csv"
+    #df = pd.read_csv("https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_South_America_24h.csv")
+    df = pd.read_csv(url)
+    #dfDate = df[df["acq_date"] == datetime.datetime.now().strftime("%Y-%m-%d")]
+    #if(len(dfDate) > 0):
+    dfDate = df
+    dfLat = dfDate[dfDate["latitude"] < -16.5]
+    dfLat2 = dfLat[dfLat["longitude"] < -69.5]
+
+    # AQUÍ SE PODRÍA AGREGAR LA INFORMACIÓN CALLE, COMUNA, PROVINCIA, REGIÓN.
+    # CALLE, COMUNA, PROVINCIA, REGIÓN (INCLUIR JSON)
+
+    dfLat2.to_csv(f"Data/{fuente[1]}/Puntos_Diarios_{fuente[1]}.csv")
+    dfLat2.to_csv(f"Data_Legacy/{fuente[1]}/Puntos_Diarios_{fuente[1]}_{datetime.datetime.now().strftime('%Y-%m-%d')}.csv")
+    
+
+    
+    return dfLat2
+
+def getJSON(fuente):
+    df = descarga(fuente)
+    features = []
+    features2 = []
+    for i, j in df.iterrows():
+        f = {'type': 'Feature', \
+            'geometry': {'type': 'Point', 'coordinates': [j["longitude"], j["latitude"]]}, \
+            'properties': {'acq_date': j["acq_date"]}}
+        features.append(f.copy())
+        f = {'acq_date': j["acq_date"],"lat":j["latitude"],"lng":j["longitude"]}
+        features2.append(f.copy())
+    salida = {"type":"FeatureCollection","features":features}
+    with open(f'Data/{fuente[1]}/heatmap_{fuente[1]}.json', 'w') as file:
+        json.dump(salida, file, indent=4)
+    with open(f'Data/{fuente[1]}/data_{fuente[1]}.json', 'w') as file:
+        json.dump(features2, file, indent=4)
+    return True
+
+def proceso():
+    for i in fuentes:
+        #descarga(i)
+        getJSON(i)
+
+if __name__ == '__main__':
+    try:
+        proceso()
+    except:
+        try:
+            proceso()
+        except:
+            error = sys.exc_info()[1]
+            print(error)
