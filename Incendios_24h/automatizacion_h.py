f944b7393f5728be7d7a7ccc85e279edb6a92875
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2022-01-23 21:23:13 -0600

    Commit

--- Incendios_24h/automatizacion_h.py
+++ Incendios_24h/automatizacion_h.py
@@ -5,12 +5,7 @@ import sys
 import datetime
 import json
 
-fuentes = [["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true",
-            "MODIS_fuente"],
-            ["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true",
-            "SUOMI_fuente"],
-            ["https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true",
-            "J1_fuente"]]
+fuentes = ["MODIS_fuente", "SUOMI_fuente", "J1_fuente"]
 
 def regiones(region):
     
@@ -31,29 +26,27 @@ def provincias(prov):
     return data['PROVINCIA'][indx]
 
 def descarga(fuente):
-    url = fuente[0]
+
     dataFuente = ''
 
-    if(fuente[1] == 'MODIS_fuente'):
+    if(fuente == 'MODIS_fuente'):
         dataFuente = 'MODIS'
 
-    if(fuente[1] == 'SUOMI_fuente'):
+    if(fuente == 'SUOMI_fuente'):
         dataFuente = 'SUOMI'
 
-    if(fuente[1] == 'J1_fuente'):
+    if(fuente == 'J1_fuente'):
         dataFuente = 'J1'
 
     print('FUENTE: ' + str(dataFuente))   
     
     today = str(datetime.datetime.today())[0:10]
 
-    dfData = pd.read_excel(url)
+    dfData = pd.read_excel('https://github.com/Sud-Austral/Fuego_LEAFLET/blob/main/SEMANARIO_INCENDIO/Consolidado/ConsolidadoPuntosFuego.xlsx?raw=true')
     dfData = dfData[dfData['Fuente'] == dataFuente]
 
     dfData['NOM_REGION'] = dfData['REGION'].apply(lambda x: regiones(x))
     dfData['NOM_PROVINCIA'] = dfData['PROVINCIA'].apply(lambda x: provincias(x))
-
-    
 
     dfLat2 = dfData
 
@@ -79,9 +72,9 @@ def getJSON(fuente):
         f = {'acq_date': j["acq_date"],"lat":j["latitude"],"lng":j["longitude"], "location": j["Locacion"], "region": j["NOM_REGION"], "provincia": j["NOM_PROVINCIA"], "comuna": j["Comuna"]}
         features2.append(f.copy())
     salida = {"type":"FeatureCollection","features":features}
-    with open(f'Incendios_24h/Data/{fuente[1]}/heatmap_{fuente[1]}.json', 'w') as file:
+    with open(f'Incendios_24h/Data/{fuente}/heatmap_{fuente}.json', 'w') as file:
         json.dump(salida, file, indent=4)
-    with open(f'Incendios_24h/Data/{fuente[1]}/data_{fuente[1]}.json', 'w') as file:
+    with open(f'Incendios_24h/Data/{fuente}/data_{fuente}.json', 'w') as file:
         json.dump(features2, file, indent=4)
     return True
 
